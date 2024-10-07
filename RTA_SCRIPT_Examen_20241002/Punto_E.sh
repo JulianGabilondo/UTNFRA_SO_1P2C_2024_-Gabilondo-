fccf19f8deb13d1175c68008f08efd874500d23c
JulianGabilondo <juliangabilondo@gmail.com>
2024-10-07 20:41:12 -0300

    Update Punto_E.sh

--- RTA_SCRIPT_Examen_20241002/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_E.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir $HOME/repogit/UTNFRA_SO_1P2C_2024_-Gabilondo-/RTA_ARCHIVOS_Examen_20241002
-grep -i "memtotal" /proc/meminfo | tee -a $HOME/repogit/UTNFRA_SO_1P2C_2024_-Gsbilondo-/RTA_ARCHIVOS_Examen_20241002/Filtro_Basico.txt
+grep -i "memtotal" /proc/meminfo | tee -a $HOME/repogit/UTNFRA_SO_1P2C_2024_-Gabilondo-/RTA_ARCHIVOS_Examen_20241002/Filtro_Basico.txt
 sudo dmidecode -t chassis | tail -n 12 | head -n 2 | tee -a $HOME/repogit/UTNFRA_SO_1P2C_2024_-Gabilondo-/RTA_ARCHIVOS_Examen_20241002/Filtro_Basico.txt
